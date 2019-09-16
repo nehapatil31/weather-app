3f16ff358695ee24713a4d9e3cfcd432b5bd98fe
Neha <nehapatil317@gmail.com>
2019-09-16 23:15:56 +0530

    Added my project

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,11 @@
 
-
-
 const weatherForm = document.querySelector('form')
 const searchElement = document.querySelector('input')
 const msgOne = document.querySelector('#message-1')
 const msgTwo = document.querySelector('#message-2')
+navigator.serviceWorker && navigator.serviceWorker.register('./sw.js').then(function (registration) {
+    console.log('Excellent, registered with scope: ', registration.scope);
+});
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 const path = require('path')
+
+
 const express = require('express')
 const hbs = require('hbs')
 const geocode = require('./utils/geocode');
